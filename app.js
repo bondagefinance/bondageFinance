0b4f3ece6d66b2fe8c54135c62af23e93b1b3e76
bondagefinance <157675424+bondagefinance@users.noreply.github.com>
2024-01-27 10:20:06 +0000

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ let web3;
 if (typeof window.ethereum !== 'undefined') {
     web3 = new Web3(window.ethereum);
 } else {
-    web3 = new Web3(new Web3.providers.HttpProvider('https://goerli.infura.io/v3/YOUR_INFURA_PROJECT_ID'));
+    web3 = new Web3(new Web3.providers.HttpProvider('https://rpc.goerli.eth.gateway.fm'));
 }
 
 // Contract ABIs and Addresses
